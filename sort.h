0db6bba86c14fdf8f75fbea12ef31d224ac9de80
Ibidapo-chad <Ibidapo-chad@gmail.com>
2021-08-08 22:41:57 -0700

    extra tasks included

--- sort.h
+++ sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -27,5 +28,14 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void lomuto_qsort(int *array, int lo, int hi, size_t size);
 int partition(int *array, int lo, int hi, size_t size);
+
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif /*SORT_H*/
